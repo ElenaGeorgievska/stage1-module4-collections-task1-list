fd9f23f8ee57bfbc147574b08a82761adef4027e
ElenaGeorgievska <elena_science@yahoo.com>
2023-09-19 18:51:56 +0200

    stage1-module4_collections-task1-list completed

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -6,30 +6,15 @@ import static java.lang.Math.abs;
 import static java.lang.Math.pow;
 
 
-//Implement the program which gets the List and sorts it in ascending order of function 5x^2+3, where x is element from List.
-// If the function value is the same for multiple elements, those elements are sorted in ascending order.
-//
-//List consists of int numbers represented as a String. Use a Comparator for sorting.
+
 public class ListSorter {
     public void sort(List<String> sourceList) {
 
-        List<String> resultsOfFunction = solveFunction(sourceList);
+        sourceList.sort(new ListComparator());
+        Collections.reverse(sourceList);
+        System.out.println((sourceList));
 
-        resultsOfFunction.sort(new ListComparator());
     }
-
-    public List<String> solveFunction(List<String> sourceList) {
-
-        List<String> result = new LinkedList<>();
-        for (int i = 0; i <= sourceList.size() - 1; i++) {
-            //int e = (int) abs((5 * pow(Integer.valueOf(sourceList.get(i)), 2) + 3));
-            int e = (int) (5 * pow((Integer.parseInt(sourceList.get(i))), 2) + 3);
-            result.add(String.valueOf(e));
-        }
-
-        return result;
-    }
-
 
     public static void main(String[] args) {
         //List<String> sourceList = Arrays.asList("-5", "-12", "0", "20", "9", "-20", "37");
@@ -44,7 +29,6 @@ public class ListSorter {
 
         ListSorter ls = new ListSorter();
         ls.sort(sourceList);
-        //System.out.println();
     }
 
 }
@@ -53,14 +37,30 @@ class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
 
-        return Integer.valueOf(a).compareTo(Integer.valueOf(b));
+        int intA = Integer.parseInt(a);
+        int intB = Integer.parseInt(b);
+
+        int calculatedA = calc(intA);
+        int calculatedB = calc(intB);
+
+        return Integer.compare(Math.abs(calculatedB), Math.abs(calculatedA));
+    }
+
+    private int calc(int x) {
+        double form = Math.pow(5 * x, 2) + 3;
+        int result = (int) form;
+        return result;
     }
 
 
+    //    @Override
+//    public int compare(String a, String b) {
+//
+//        //return Integer.valueOf(a).compareTo(Integer.valueOf(b));
+//        int intA = Integer.parseInt(a);
+//        int intB = Integer.parseInt(b);
+//        return Integer.compare(Math.abs(intB), Math.abs(intA));
+//    }
+
 }
 
-//Input
-//["-5", "-12", "0", "20", "9", "-20", "37"]
-
-//Output
-//["0", "-5", "9", "-12", "-20", "20", "37"]
